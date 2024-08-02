c28aa25f0834b359ec1b7dad240c39a2037385a4
michaelk0217 <temin.kim@gmail.com>
2024-08-02 19:33:50 -0400

    Initial Commit

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,16 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack() {
+            CardView(isFaceUp: true)
+            CardView()
+            
         }
+        .foregroundColor(.indigo)
         .padding()
+        
     }
 }
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack() {
+            if isFaceUp{
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 10)
+                
+                Text("🐶").font(.largeTitle)
+            } else{
+                RoundedRectangle(cornerRadius: 12)            }
+            
+        }
+        
+    }
+}
+
 
 #Preview {
     ContentView()
