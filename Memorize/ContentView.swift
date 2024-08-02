6128dd1f899672cae2aed32ae7f1bd1f714516a3
michaelk0217 <temin.kim@gmail.com>
2024-08-02 12:18:54 -0400

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Michael Kim on 8/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
