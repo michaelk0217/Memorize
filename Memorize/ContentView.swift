c33dd2091d92a5553e49df33a9645360427a4a62
michaelk0217 <temin.kim@gmail.com>
2024-08-02 20:42:52 -0400

    Buttons and crash protection

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,29 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis: Array<String> = ["🐶",  "🐱", "🐼", "🐯"]
+    let emojis: Array<String> = ["🐶", "🐱", "🐼", "🐯", "🦁", "🐧", "🐮", "🐸", "🐵", "🐔", "🐝", "🦄"]
+    
+    @State var cardCount: Int = 4
     
     var body: some View {
-        HStack() {
-            ForEach(emojis.indices, id: \.self){ index in
-                CardView(content: emojis[index])
+        VStack(){
+            HStack() {
+                ForEach(0..<cardCount, id: \.self){ index in
+                    CardView(content: emojis[index])
+                }
+                
             }
-        }
-        .foregroundColor(.indigo)
-        .padding()
+            .foregroundColor(.green)
+            
+            
+            HStack() {
+                Button(action: {
+                    if cardCount > 1{
+                        cardCount -= 1
+                    }
+                }, label: {
+                    Image(systemName: "minus.square.fill")
+                })
+                Spacer()
+                Button(action: {
+                    if cardCount < emojis.count{
+                        cardCount += 1
+                    }
+                    
+                }, label: {
+                    Image(systemName: "plus.square.fill")
+                })
+                
+            }
+            .imageScale(.large)
+            .font(.largeTitle)
+            
+        }.padding()
+        
     }
 }
 
 struct CardView: View {
     let content: String
-    @State var isFaceUp: Bool = false
+    @State var isFaceUp: Bool = true
     
     var body: some View {
         ZStack() {
             let base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
             if isFaceUp{
                 base.fill(.white)
-                base.strokeBorder(lineWidth: 10)
+                base.strokeBorder(lineWidth: 5)
                 Text(content).font(.largeTitle)
             } else{
                 base.fill()
